f39d7ec8eb253264de8534b6964092218b3da5b9
Cesar Felipe <cesarfelipe.5@gmail.com>
2023-04-09 22:22:45 -0300

    Update connection

--- medclould_back/src/database/connection.js
+++ medclould_back/src/database/connection.js
@@ -2,13 +2,13 @@ const config = {
     client: 'mysql2',
 
     connection: {
-        host: "localhost",
-        user: "cesar",
-        password: "admin",
+        host: "medcloud.cksqvlsjpsrb.us-east-1.rds.amazonaws.com",
+        user: "admin",
+        password: "Cesar280197",
         database: "medcloud",
     },
-}
+};
 
 const knex = require('knex')(config);
 
-module.exports = knex
+module.exports = knex;
